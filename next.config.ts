eb442bad2e2f7ddff1a110a033c94a859d6aefe1
alyssacaples <alyssacaples@outlook.com>
2026-03-12 20:42:56 -0700

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    output: "export",
+    basePath: "/2026-oscars-leaderboard",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
